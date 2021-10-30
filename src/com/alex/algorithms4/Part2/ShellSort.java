5ef2f75765232d902da347adb09ae129747dfdf0
何进 <hejin@keloop.vip>
2021-10-30 10:58:01 +0800

    first commit

---
+++ src/com/alex/algorithms4/Part2/ShellSort.java
@@ -0,0 +1,49 @@
+package com.alex.algorithms4.Part2;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+class ShellSort {
+
+    public static void sort(Comparable[] a) {
+
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + "\t");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int N = 4000;
+        Double[] a = new Double[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.uniform();
+        }
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+
+}
